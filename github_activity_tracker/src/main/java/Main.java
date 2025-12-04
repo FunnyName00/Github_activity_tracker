7bde556d2a8dc7d41cab971315e1aa95cb806b8e
FunnyName0_0 <emi.houdart@student.uclouvain.be>
2025-12-04 12:38:14 +0100

    Added user fetching infos with html API

--- github_activity_tracker/src/main/java/Main.java
+++ github_activity_tracker/src/main/java/Main.java
@@ -1,4 +1,34 @@
-public class Main {
 
-    
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            URL url = new URL("https://api.github.com/users/FunnyName00");
+            HttpURLConnection con = (HttpURLConnection) url.openConnection();
+
+            con.setRequestMethod("GET");
+
+            int status = con.getResponseCode();
+            System.out.println("Status: " + status);
+
+            BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+            String inputLine;
+            StringBuilder content = new StringBuilder();
+
+            while ((inputLine = in.readLine()) != null) {
+                content.append(inputLine + " \n");
+            }
+
+            in.close();
+            con.disconnect();
+
+            System.out.println("Response: " + content.toString());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
